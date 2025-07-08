fe414a9b980c8e0491fd0e6fe3eb581a622974f5
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-07-08 17:21:59 +0100

    Adapt admin blog view into standard user

--- src/components/blogs/blogPage.jsx
+++ src/components/blogs/blogPage.jsx
@@ -1,11 +1,8 @@
-import PageTitleBar from '../pageTitleBar';
 import Container from '../container';
-import BlogForm from './blogForm';
-import DeleteButton from '../deleteButton';
 import CommentsList from '../comments/commentsList';
 import CommentForm from '../comments/commentForm';
 import { GeneralButton } from '../styles/buttons';
-import { MobileMarginContainer } from '../styles/mainPage';
+import BlogHeader from './blogHeader';
 
 import { AccessContext } from '../../contexts/AppContexts';
 import useRefresh from '../../hooks/useRefresh';
@@ -13,49 +10,19 @@ import authFetch from '../../ext/authFetch';
 import responseToJsend from '../../ext/responseToJsend';
 
 import { useContext, useState } from 'react';
-import { useLoaderData, useNavigate, useRouteError } from 'react-router';
+import { useLoaderData, useRouteError } from 'react-router';
+import dateTimeFormatter from '../../ext/dateTimeFormatter';
 
 export default function BlogPage() {
   const { blog, comments } = useLoaderData();
-  const navigate = useNavigate();
   const refresh = useRefresh();
 
   const accessRef = useContext(AccessContext);
 
   const [isFetching, setIsFetching] = useState(false);
-  const [blogValidationErrors, setBlogValidationErrors] = useState(null);
   const [commentValidationErrors, setCommentValidationErrors] = useState(null);
   const [error, setError] = useState(useRouteError());
   const [isCreatingComment, setIsCreatingComment] = useState(false);
-
-  async function saveChanges(event) {
-    event.preventDefault();
-    setIsFetching(true);
-    const { response, fetchError } = await authFetch(
-      `${import.meta.env.VITE_SERVER_URL}/admin/blogs/${blog.id}`,
-      accessRef,
-      {
-        method: 'put',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: new URLSearchParams(new FormData(event.target)),
-      },
-    );
-    if (fetchError) setError(fetchError);
-    else {
-      const { status, data, error } = await responseToJsend(response);
-      setError(error);
-      setBlogValidationErrors(data?.validationErrors);
-      switch (status) {
-        case 'success': {
-          navigate('/blogs');
-          break;
-        }
-      }
-    }
-    setIsFetching(false);
-  }
 
   async function createComment(event) {
     event.preventDefault();
@@ -90,61 +57,49 @@ export default function BlogPage() {
   return (
     <main>
       {blog && (
-        <>
-          <MobileMarginContainer>
-            <PageTitleBar title={blog.title}>
-              <DeleteButton
-                url={`${import.meta.env.VITE_SERVER_URL}/admin/blogs/${blog.id}`}
-                onDelete={() => navigate('/blogs')}
+        <Container>
+          <BlogHeader as="h2">{blog.title}</BlogHeader>
+          <small>
+            By {blog.owner.name} at{' '}
+            {blog.publishedAt
+              ? dateTimeFormatter.format(new Date(blog.publishedAt))
+              : 'never'}
+          </small>
+
+          <p>{blog.body}</p>
+          <h3>Comments {comments?.length > 0 ? `(${comments.length})` : ''}</h3>
+          {isCreatingComment ? (
+            <>
+              <CommentForm
+                buttonText="Submit"
+                initialValues={{ text: '' }}
+                isFetching={isFetching}
+                validationErrors={commentValidationErrors}
+                onSubmit={createComment}
               >
-                Delete
-              </DeleteButton>
-            </PageTitleBar>
-          </MobileMarginContainer>
-          <Container>
-            <BlogForm
-              buttonText={'Save changes'}
-              initialValues={blog}
-              isFetching={isFetching}
-              validationErrors={blogValidationErrors}
-              onSubmit={saveChanges}
-            />
-            <h3>
-              Comments {comments?.length > 0 ? `(${comments.length})` : ''}
-            </h3>
-            {isCreatingComment ? (
-              <>
-                <CommentForm
-                  buttonText="Submit"
-                  initialValues={{ text: '' }}
-                  isFetching={isFetching}
-                  validationErrors={commentValidationErrors}
-                  onSubmit={createComment}
+                <GeneralButton
+                  type="button"
+                  onClick={() => setIsCreatingComment(false)}
                 >
-                  <GeneralButton
-                    type="button"
-                    onClick={() => setIsCreatingComment(false)}
-                  >
-                    Cancel
-                  </GeneralButton>
-                </CommentForm>
-              </>
-            ) : (
-              <GeneralButton
-                type="button"
-                onClick={() => setIsCreatingComment(true)}
-              >
-                Add comment
-              </GeneralButton>
-            )}
-            <CommentsList
-              comments={comments}
-              onReply={refresh}
-              onDelete={refresh}
-            />
-            {error && <p>{error.message}</p>}
-          </Container>
-        </>
+                  Cancel
+                </GeneralButton>
+              </CommentForm>
+            </>
+          ) : (
+            <GeneralButton
+              type="button"
+              onClick={() => setIsCreatingComment(true)}
+            >
+              Add comment
+            </GeneralButton>
+          )}
+          <CommentsList
+            comments={comments}
+            onReply={refresh}
+            onDelete={refresh}
+          />
+          {error && <p>{error.message}</p>}
+        </Container>
       )}
     </main>
   );
--- src/components/blogs/blogsListBlog.jsx
+++ src/components/blogs/blogsListBlog.jsx
@@ -1,13 +1,9 @@
 import ListItemButtonsContainer from '../listItemButtonsContainer';
 import { GeneralButton } from '../styles/buttons';
+import BlogHeader from './blogHeader';
 import DeleteButton from '../deleteButton';
 import dateTimeFormatter from '../../ext/dateTimeFormatter';
 import { Link } from 'react-router';
-import styled from 'styled-components';
-
-const BlogHeader = styled.h3`
-  margin-bottom: 0;
-`;
 
 export default function BlogsListBlog({ blog, onDelete }) {
   return (
