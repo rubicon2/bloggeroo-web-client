b0f514cb405efb3a95cb01ebe9d8ee0c24c0c898
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-07-09 17:15:47 +0100

    Add root errorBoundary

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -30,6 +30,7 @@ export default function AppRouter() {
       {
         path: '/',
         Component: Root,
+        ErrorBoundary: ErrorPage,
         children: [
           {
             ErrorBoundary: ErrorPage,
