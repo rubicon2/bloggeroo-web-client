aae26ef8e92c6b28f2092448ae08d7d7964ddd5e
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-07-09 11:16:38 +0100

    Create unstyledLink for blogs in blogsList page

---
+++ src/components/unstyledLink.jsx
@@ -0,0 +1,8 @@
+import { Link } from 'react-router';
+import styled from 'styled-components';
+
+const UnstyledLink = styled(Link)`
+  color: var(--theme-text-color);
+`;
+
+export default UnstyledLink;
