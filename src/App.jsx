c5a9b66ee3283cb4f7f29be0a9ed8fbbd83aa054
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-07-10 16:05:41 +0100

    Fetch new access token upon app start, if user is logged in

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import AppRouter from './AppRouter';
 import { AccessContext, UserContext } from './contexts/AppContexts';
+import fetchAccess from './ext/fetchAccess';
 import useLoginState from './hooks/useLoginState';
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useLoginState();
   const accessRef = useRef(null);
+
+  // Initialize accessRef - fetch a new access token if user is already logged in.
+  useEffect(() => {
+    async function getAccess() {
+      accessRef.current = await fetchAccess();
+    }
+    getAccess();
+  }, []);
+
   return (
     <AccessContext value={accessRef}>
       <UserContext value={{ isLoggedIn, setIsLoggedIn }}>
