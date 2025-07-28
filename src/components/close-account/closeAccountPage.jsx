4dc46d206c5047088c1907f0a019c4a566720080
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-07-28 14:05:18 +0100

    Fix: user that has just deleted their account stays logged in

--- src/components/close-account/closeAccountPage.jsx
+++ src/components/close-account/closeAccountPage.jsx
@@ -1,10 +1,14 @@
 import PageTitleBar from '../pageTitleBar';
 import Container from '../container';
 import { useLoaderData, useRouteError } from 'react-router';
+import { useContext } from 'react';
+import { UserContext } from '../../contexts/AppContexts';
 
 export default function CloseAccountPage() {
   const { message } = useLoaderData();
   const error = useRouteError();
+  const { isLoggedIn, setIsLoggedIn } = useContext(UserContext);
+  if (isLoggedIn) setIsLoggedIn(false);
   return (
     <main>
       <PageTitleBar title="Close Account" />
