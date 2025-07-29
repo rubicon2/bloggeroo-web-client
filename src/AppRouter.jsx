e5358e6bbfc9d2170faf3d5d8c2664b984dc0cfb
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-07-29 11:06:09 +0100

    Remove redundant comment that pertained to an old version before react ref was used to store access token

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -25,9 +25,6 @@ import { AccessContext } from './contexts/AppContexts';
 import { useContext, useMemo } from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router';
 
-// This re-generates the browser router every time the state (i.e. access token) changes. Not great.
-// But the only way I could find to get the access token into the loader was by currying the access token
-// into generated loader functions. So when the access token changes, the loader functions need to be re-generated.
 export default function AppRouter() {
   const accessRef = useContext(AccessContext);
   const router = useMemo(() => {
