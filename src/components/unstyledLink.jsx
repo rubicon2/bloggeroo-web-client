0926915accb63d64d93d702f9aa2a444a67e304b
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-08-01 17:54:26 +0100

    Fix: blogListBlog link p font not consistent with admin client

--- src/components/unstyledLink.jsx
+++ src/components/unstyledLink.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 
 const UnstyledLink = styled(Link)`
   color: var(--theme-text-color);
+  font-weight: var(--std-font-weight);
 `;
 
 export default UnstyledLink;
