74a40eda49c79e8500d28e51bcc766c8aa62b3f2
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2026-01-05 13:56:40 +0000

    Improve styling of MarkdownBlog component
    Ensure header margins scale with font size, and set
    a max-width for paragraphs to make them more comfo-
    rtable to read.

--- src/components/blogs/markdownBlog.jsx
+++ src/components/blogs/markdownBlog.jsx
@@ -3,32 +3,39 @@ import styled from 'styled-components';
 
 const H1 = styled.h3`
   font-size: 2rem;
+  margin: 1em 0;
   border-bottom: 2px solid #00000020;
 `;
 
 const H2 = styled.h4`
   font-size: 1.6rem;
+  margin: 1em 0;
   border-bottom: 2px solid #00000020;
 `;
 
 const H3 = styled.h5`
   font-size: 1.4rem;
+  margin: 1em 0;
 `;
 
 const H4 = styled.h6`
   font-size: 1.2rem;
+  margin: 1em 0;
 `;
 
 const H5 = styled.h6`
   font-size: 1rem;
+  margin: 1em 0;
 `;
 
 const H6 = styled.h6`
   font-size: 0.8rem;
+  margin: 1em 0;
 `;
 
 const PreserveWhitespaceP = styled.p`
   white-space-collapse: preserve;
+  max-width: 50ch;
 `;
 
 const markdownComponents = {
